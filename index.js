e7f743cac546474ea23fa8ac8c29e00f734ce9a4
Gleb Shipanov <gcreego@gmail.com>
2020-04-21 21:36:59 +0500

    CV v 1.0.0

---
+++ index.js
@@ -0,0 +1,10 @@
+var body = document.querySelector('body');
+var toggle = document.querySelector('.toggle');
+
+if (body && toggle) {
+  toggle.addEventListener('change', e => {
+    body.classList.toggle('theme-dark');
+  });
+}
+
+
